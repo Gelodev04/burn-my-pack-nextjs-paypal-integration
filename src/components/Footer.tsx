cf314d5ffbd8d99cce0b5547a5c99fbe9f9ec986
gelo <gelodev83@gmail.com>
2025-05-06 15:17:29 +0800

    updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,29 +3,30 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer className="bg-[#333333] text-[12px] text-white px-4 py-7">
-      <div className="space-y-10 border-b pb-[1.5rem] border-[#515151]">
-          <div>
-            <h6 className="text-[15px] font-bold">Contact Us</h6>
-            <div className="mt-2">
-              <p>Have questions about the method?</p>
-              <p>Email: support@quitsmart.com</p>
-            </div>
+      <div className="max-w-[900px] mx-auto">
+          <div className="space-y-10 border-b pb-[1.5rem] border-[#515151] flex ">
+              <div className="w-1/2">
+                <h6 className="text-[15px] font-bold">Contact Us</h6>
+                <div className="mt-2">
+                  <p>Have questions about the method?</p>
+                  <p>Email: support@quitsmart.com</p>
+                </div>
+              </div>
+              <div className="w-1/2">
+                <h6 className="text-[15px] font-bold">Important Notice</h6>
+                <div className="mt-2">
+                  <p>
+                    This method is based on personal experience and psychological
+                    principles. Results may vary. Consult your healthcare provider
+                    before starting any smoking cessation program.
+                  </p>
+                </div>
+              </div>
           </div>
-          <div>
-            <h6 className="text-[15px] font-bold">Important Notice</h6>
-            <div className="mt-2">
-              <p>
-                This method is based on personal experience and psychological
-                principles. Results may vary. Consult your healthcare provider
-                before starting any smoking cessation program.
-              </p>
-            </div>
-          </div>
+          <p className="text-center mt-[1.5rem]">
+          © {new Date().getFullYear()} QuitSmart. All rights reserved.
+          </p>
       </div>
-
-      <p className="text-center mt-[1.5rem]">
-      © {new Date().getFullYear()} QuitSmart. All rights reserved.
-      </p>
     </footer>
   );
 };
